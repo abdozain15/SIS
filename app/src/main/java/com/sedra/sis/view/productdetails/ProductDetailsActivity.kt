106f38ace2de5b268403424e7d8460ee8b52ed1d
KhaledAlramam <khaledalramam@gmail.com>
2021-04-27 13:45:07 +0200

    new design

---
+++ app/src/main/java/com/sedra/sis/view/productdetails/ProductDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.sedra.sis.view.productdetails
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sedra.sis.R
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
